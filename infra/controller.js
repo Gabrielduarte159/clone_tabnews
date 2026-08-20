faec63d88fde4fa2b4c3e8bfdb1dd124a6637db6
Gabriel <gabrielvduarte9@gmail.com>
2026-08-20 10:04:40 -0300

    feat: implement session creation

--- infra/controller.js
+++ infra/controller.js
@@ -3,18 +3,24 @@ import {
   MethodNotAllowedError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
+
 function onErrorHandler(error, request, response) {
-  if (error instanceof ValidationError || error instanceof NotFoundError) {
+  if (
+    error instanceof ValidationError ||
+    error instanceof NotFoundError ||
+    error instanceof UnauthorizedError
+  ) {
     return response.status(error.statusCode).json(error);
   }
+
   const publicErrorObject = new InternalServerError({
-    statusCode: error.statusCode,
     cause: error,
   });
 
@@ -29,4 +35,5 @@ const controller = {
     onError: onErrorHandler,
   },
 };
+
 export default controller;
